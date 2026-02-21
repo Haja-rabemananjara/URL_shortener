52d447a3ef12d3feb47a5bfc4434a2f876c81738
Haja <hajatiana.rabemananjara@epitech.eu>
2026-02-21 18:54:40 +0100

    feat: class validator and class transformer

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
+/**
+ * The main application module. This is where you can import other modules, declare controllers, and provide services.
+ * In a real application, you would typically have multiple modules for different features (e.g., UserModule, AuthModule, etc.).
+ */
 @Module({
-  imports: [],
+  imports: [PrismaModule], // Importing the PrismaModule to make the PrismaService available in this module.
   controllers: [AppController],
   providers: [AppService],
 })
