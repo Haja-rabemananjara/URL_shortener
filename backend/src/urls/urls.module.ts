b2ba8b208c1f83926cc5fcdc3e11f4c63e68122c
Haja <hajatiana.rabemananjara@epitech.eu>
2026-02-21 19:20:17 +0100

    fix: main.ts

--- backend/src/urls/urls.module.ts
+++ backend/src/urls/urls.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UrlsController } from './urls.controller';
 import { UrlsService } from './urls.service';
 import { UrlsController } from './urls.controller';
 
